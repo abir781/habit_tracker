0c7d3d10eccedd02925c72858be28cbeb74d6072
abir781 <hasan.abir3176@gmail.com>
2026-03-01 10:16:53 +0600

    first commit

---
+++ src/app/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+
+            <p>it's just me and no one is allowed here</p>
+            
+        </div>
+    );
+};
+
+export default Banner;
