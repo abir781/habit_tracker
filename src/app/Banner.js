e57d816c1c9d4fb69c5c2f2824893b4d809a1dd8
abir781 <hasan.abir3176@gmail.com>
2026-03-02 08:25:07 +0600

    kkkssss

--- src/app/Banner.js
+++ src/app/Banner.js
@@ -3,11 +3,25 @@ import React from 'react';
 import Navbar from './Navbar';
 import BlurIn from './Blurin';
 import FadeInButton from './Fadeinbutton';
+import { Archivo_Black } from 'next/font/google';
+
+
+//   const bungee = Bungee({
+//   subsets: ["latin"],
+//   weight: ["400"],
+// });
+
+const archivo = Archivo_Black({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+    
 
 const Banner = () => {
-    
+
+
     return (
-        <div className="bg-[url('/next.jpg')] min-h-screen bg-no-repeat bg-cover relative">
+        <div className={`bg-[url('/next.jpg')] min-h-screen bg-no-repeat bg-cover relative ${archivo.className} tracking-widest`}>
 
              <div className="absolute inset-0 bg-black/20"></div>
 
@@ -20,17 +34,17 @@ const Banner = () => {
 
                
 
-                        <div className='flex justify-center flex-col items-center min-h-screen'>
+                        <div className='flex justify-center flex-col items-center min-h-screen '>
 
                           <BlurIn>
 
-                                <p className='text-5xl mb-4 font-bold bg-gradient-to-r from-teal-300 via-cyan-300 to-emerald-300 bg-clip-text text-transparent'>
+                                <p className='text-5xl mb-6 font-bold bg-gradient-to-r from-teal-300 via-cyan-300 to-emerald-300 bg-clip-text text-transparent'>
     Make Discipline Your Superpower.
   </p>
-  <div className='max-w-[600px] text-center mb-4'>
+  <div className='max-w-[600px] text-center mb-6  mx-auto'>
   
 
-         <p className='text-xl text-transform: uppercase font-bold text-cyan-200'>
+         <p className='text-xl text-transform: uppercase font-bold text-cyan-200 '>
       Stay committed, track your streaks, and unlock your potential.
     </p>
 
--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,9 @@ import Banner from "./Banner";
 export default function Home() {
   return (
 
-    <div>
-      <Banner></Banner>
-    </div>
+  <div >
+  <Banner />
+</div>
      
   );
 }
